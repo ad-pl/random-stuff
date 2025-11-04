5b129297e8347318357ef33e3bd54e05ee86b517
ad <224856236+ad-pl@users.noreply.github.com>
2025-11-04 22:18:28 +0000

    Update suv7.py

--- suv7.py
+++ suv7.py
@@ -1,23 +1,16 @@
 import time
 from random import getrandbits
 from uuid import UUID
-
-def bitmasker(x:int,size:int)->int:
-	return (2**size-1)&x
-def uuidv7(shift:int=0,rand_a:int=getrandbits(12),rand_b:int=getrandbits(62))->UUID:
-	result=(int(time.time()*1000) + shift).to_bytes(6,'big')
-	result.extend((7<<12|bitmasker(rand_a,12)).to_bytes(2))
-	result.extend((2<<62|bitmasker(rand_b,62)).to_bytes(8))
+def uuid7(shift:int=None,rand_a:int=None,rand_b:int=None)->UUID:
+	rand_a=rand_a if rand_a!=None else getrandbits(12)
+	rand_b=rand_b if rand_b!=None else getrandbits(62)
+	shift=shift if shift!=None else 0
+	result=list((int(time.time()*1000)+shift).to_bytes(6,'big'))
+	result.extend((7<<12|(2**12-1&rand_a)).to_bytes(2))
+	result.extend((2<<62|(2**62-1&rand_b)).to_bytes(8))
 	return UUID(bytes=bytes(result))
-def uuidv8(custom_a:int,custom_b:int,custom_c:int)->UUID:
-	custom_a=bitmasker(custom_a,48)
-	custom_b=bitmasker(custom_b,12)
-	custom_c=bitmasker(custom_c,62)
-	result=list(custom_a.to_bytes(6))
-	result.extend((8<<12|bitmasker(custom_b,12)).to_bytes(2))
-	result.extend((2<<62|bitmasker(custom_c,62)).to_bytes(8))
-# why no 1-6:
-# 1,3,4,5: python already has it
-# 2: why do you need it
-# 6: 'Systems that do not involve legacy UUIDv1
-#     SHOULD use UUIDv7 (Section 5.7) instead.' - rfc-editor.org
+def uuid8(a:int,b:int,c:int)->UUID:
+	result=list(a.to_bytes(6))
+	result.extend((8<<12|2**12-1&a).to_bytes(2))
+	result.extend((2<<62|2**12-1&b).to_bytes(8))
+	return UUID(bytes=bytes(result))
