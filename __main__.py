8f5e31206df32836e442d61cea40fda4ba685831
ad <224856236+ad-pl@users.noreply.github.com>
2025-11-05 11:57:53 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
-from random import getrandbits
-from uuid import UUID
-from time import time
-def uuid7(shift:int=None,rand_a:int=None,rand_b:int=None)->UUID:
-	return UUID(bytes=bytes(int(time()*1000+(shift or 0)).to_bytes(6)+((7<<12|(2**12-1&(rand_a or getrandbits(12)))).to_bytes(2))+((2<<62|(2**62-1&(rand_b or getrandbits(62)))).to_bytes(8))))
-def uuid8(a:int,b:int,c:int)->UUID:
-	return UUID(bytes=bytes(a.to_bytes(6)+((8<<12|2**12-1&a).to_bytes(2))+(2<<62|2**12-1&b).to_bytes(8)))
+import random, uuid, time
+def uuid7(shift:int=None,rand_a:int=None,rand_b:int=None)->uuid.UUID:
+	return uuid.UUID(bytes=bytes((int(time.time()*1000+(shift or 0))&2**48-1).to_bytes(6)+((7<<12|(2**12-1&(rand_a or random.getrandbits(12)))).to_bytes(2))+((2<<62|(2**62-1&(rand_b or random.getrandbits(62)))).to_bytes(8))))
+def uuid8(a:int,b:int,c:int)->uuid.UUID:
+	return uuid.UUID(bytes=bytes((2**48-1&a).to_bytes(6)+((8<<12|2**12-1&b).to_bytes(2))+(2<<62|2**62-1&c).to_bytes(8)))
