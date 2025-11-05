9a58a2f7a2d8aef118a949b154652b80f698c071
ad <224856236+ad-pl@users.noreply.github.com>
2025-11-05 11:39:33 +0000

    Update and rename suv7.py to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,10 +1,7 @@
-import time
 from random import getrandbits
 from uuid import UUID
+from time import time
 def uuid7(shift:int=None,rand_a:int=None,rand_b:int=None)->UUID:
-	rand_a=rand_a or getrandbits(12)
-	rand_b=rand_b or getrandbits(62)
-	shift=shift or 0
-	return UUID(bytes=bytes(int(time.time()*1000+shift).to_bytes(6)+((7<<12|(2**12-1&rand_a)).to_bytes(2))+((2<<62|(2**62-1&rand_b)).to_bytes(8))))
+	return UUID(bytes=bytes(int(time()*1000+(shift or 0)).to_bytes(6)+((7<<12|(2**12-1&(rand_a or getrandbits(12)))).to_bytes(2))+((2<<62|(2**62-1&(rand_b or getrandbits(62)))).to_bytes(8))))
 def uuid8(a:int,b:int,c:int)->UUID:
 	return UUID(bytes=bytes(a.to_bytes(6)+((8<<12|2**12-1&a).to_bytes(2))+(2<<62|2**12-1&b).to_bytes(8)))
